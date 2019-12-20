8d414cb5150621191f724b5c3c3a111443c7e750
Fereydoun Memarzanjany <VioletVillain@outlook.com>
2019-12-20 18:07:25 +0330

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{WebGl2RenderingContext};
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let context = canvas.get_context("webgl2")?.unwrap().dyn_into::<WebGl2RenderingContext>()?;
+
+    context.clear_color(0.0, 0.0, 0.0, 1.0);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    Ok(())
+}
