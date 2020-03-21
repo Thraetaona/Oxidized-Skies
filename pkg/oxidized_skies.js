3fe1db9f6973ddd4e858ffad5c4d8c5b111c8370
Fereydoun Memarzanjany <F_Memarzanjany@outlook.com>
2020-03-21 19:51:57 +0430

    Update oxidized_skies.js

--- pkg/oxidized_skies.js
+++ pkg/oxidized_skies.js
@@ -1,5 +1,4 @@
-let wasm;
-let memory;
+import * as wasm from './oxidized_skies_bg.wasm';
 
 /**
 */
@@ -7,32 +6,5 @@ export function main() {
     wasm.main();
 }
 
-async function load(module, imports, maybe_memory) {
-    try {
-        return await WebAssembly.instantiateStreaming(module, imports);
-    } catch (e) {
-         throw e;
-    }
-}
-
-async function init(input, maybe_memory) {
-    if (typeof input === 'undefined') {
-        input = import.meta.url.replace(/\.js$/, '_bg.wasm');
-    }
-    const imports = {};
-
-
-    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
-        input = fetch(input);
-    }
-
-    const { instance, module } = await load(await input, imports, maybe_memory);
-
-    wasm = instance.exports;
-    init.__wbindgen_wasm_module = module;
-    wasm.__wbindgen_start();
-    return wasm;
-}
-
-export default init;
+wasm.__wbindgen_start();
 
